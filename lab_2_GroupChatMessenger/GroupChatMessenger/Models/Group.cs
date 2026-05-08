31c016cb9e51714024d2285c5c1e0b3ef6d3efd6
Margaryta0 <smalmargarita0@gmail.com>
2026-05-08 08:28:32 +0300

    delete

--- lab_2_GroupChatMessenger/GroupChatMessenger/Models/Group.cs
+++
@@ -1,9 +0,0 @@
-namespace GroupChatMessenger.Models;
-
-public class Group
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public string Name { get; set; } = string.Empty;
-    public List<string> MemberIds { get; set; } = new();
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
--- lab_2_GroupChatMessenger/GroupChatMessenger/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace GroupChatMessenger.Models;
-
-public class User
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString(); 
-    public string Name { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
